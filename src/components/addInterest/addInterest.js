68e4a279c2af32ef92a832f1a4a7cc9365d9dd89
WaysideTester <dylanmurray211@outlook.com>
2018-12-29 20:31:28 -0600

    adding inpu fields

--- src/components/addInterest/addInterest.js
+++ src/components/addInterest/addInterest.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import {
+  FormGroup,
+  Label,
+  Input,
+} from 'reactstrap';
 import './addInterest.scss';
 
 class addInterest extends React.Component {
   render() {
     return (
-      <h2>Add Interest section</h2>
+      <div>
+        <h2>Add Interest section</h2>
+        <FormGroup>
+          <Input type="password" name="password" id="examplePassword" placeholder="Task to Complete" />
+        </FormGroup><FormGroup>
+          <Input type="password" name="password" id="examplePassword" placeholder="Link to Task" />
+        </FormGroup>
+        <FormGroup check>
+          <Label check>
+            <Input type="checkbox" />
+            Check me out
+          </Label>
+        </FormGroup>
+      </div>
     );
   }
 }
